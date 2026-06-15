a47444fef1c01acc800d283eb5293bffbfdf48c9
Vanshika Srivastava <vs905@snu.edu.in>
2026-06-15 22:21:29 +0530

    Tested deskewing

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -15,15 +15,19 @@ def enhance_contrast(image: np.ndarray) -> np.ndarray:
     return clahe.apply(image)
 
 
-def deskew(image: np.ndarray) -> np.ndarray:
+def detect_skew_angle(image: np.ndarray) -> float:
     coords = np.column_stack(np.where(image < 128))
     angle = cv2.minAreaRect(coords)[-1]
     if angle < -45:
         angle = 90 + angle
+    return angle
 
+
+def deskew(image: np.ndarray) -> np.ndarray:
+    angle = detect_skew_angle(image)
     height, width = image.shape[:2]
     center = (width // 2, height // 2)
-    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    matrix = cv2.getRotationMatrix2D(center, -angle, 1.0)
     return cv2.warpAffine(
         image, matrix, (width, height), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
     )
